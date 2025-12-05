a9453af2a26b68481ddb02855f70ca7f56f663f3
thetacola <alex@consus.fun>
2025-12-04 20:16:09 -0500

    added some unit tests

---
+++ src/test/java/oling/NewUnitTests.java
@@ -0,0 +1,73 @@
+package oling;
+
+import net.oijon.oling.Parser;
+import net.oijon.oling.datatypes.grammar.Gloss;
+import net.oijon.oling.datatypes.grammar.GlossList;
+import net.oijon.oling.datatypes.language.Language;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class NewUnitTests {
+
+    /**
+     * Grammar Tests
+     */
+    @Test
+    void glossTest() {
+        Gloss gloss = new Gloss("test", "test meaning");
+        Gloss gloss2 = new Gloss(gloss);
+        String expectedGlossStr = "===Gloss Start===\nabb:test\nmeaning:test meaning\n===Gloss End===";
+        assertEquals(expectedGlossStr, gloss.toString());
+        assertEquals(expectedGlossStr, gloss2.toString());
+    }
+
+    @Test
+    void glossListTest() {
+        GlossList gl = new GlossList("Test list");
+        // TODO: automatically sort gloss lists on adding, then test for arbitrary order
+        gl.add(new Gloss("1", "first person"));
+        gl.add(new Gloss("2", "second person"));
+        gl.add(new Gloss("3", "third person"));
+
+        String expectedListStr = "===GlossList Start===\nname:Test list\n===Glosses Start===\n===Gloss Start===\nabb:1\n" +
+                "meaning:first person\n===Gloss End===\n===Gloss Start===\nabb:2\nmeaning:second person\n" +
+                "===Gloss End===\n===Gloss Start===\nabb:3\nmeaning:third person\n===Gloss End===\n===Glosses End===\n" +
+                "===GlossList End===";
+
+        assertEquals(expectedListStr, gl.toString());
+    }
+
+    /**
+     * Language tests
+     */
+
+    @Test
+    void testLanguage() {
+        try {
+            Parser parser = new Parser(Paths.get(getClass().getClassLoader().getResource("testish.language").toURI()).toFile());
+            Language testLang = parser.parseLanguage();
+            testLang.toFile(new File(System.getProperty("user.home") + "/.oling/testish.language"));
+            Parser parser2 = new Parser(new File(System.getProperty("user.home") + "/.oling/testish.language"));
+            Language compareLanguage = parser2.parseLanguage();
+            testLang.setOrtho(compareLanguage.getOrtho()); // ortho not created in testlang, is in comparelang
+            assertEquals(testLang, compareLanguage);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    // language properties
+
+    /**
+     * Lexicon tests
+     */
+
+
+
+}

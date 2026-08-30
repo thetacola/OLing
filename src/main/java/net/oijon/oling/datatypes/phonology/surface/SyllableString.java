fbc3c9fbf4184ace9f45f5a8550dde216c280c34
thetacola <alex@consus.fun>
2026-08-29 22:55:56 -0400

    added syllable strings

--- src/main/java/net/oijon/oling/datatypes/phonology/surface/SyllableString.java
+++ src/main/java/net/oijon/oling/datatypes/phonology/surface/SyllableString.java
@@ -141,4 +141,199 @@ public class SyllableString {
 		sylls = Arrays.copyOf(s.sylls, s.sylls.length);
 	}
 	
+	public Syllable syllableAt(int i) {
+		return sylls[i];
+	}
+	
+	public SyllableString concat(SyllableString s) {
+		Syllable[] newArr = new Syllable[this.sylls.length + s.sylls.length];
+		for (int i = 0; i < this.sylls.length; i++) {
+			newArr[i] = this.sylls[i];
+		}
+		for (int i = 0; i < s.sylls.length; i++) {
+			newArr[this.sylls.length + i] = s.sylls[i];
+		}
+		return new SyllableString(newArr);
+	}
+	
+	public SyllableString concat(Syllable s) {
+		Syllable[] newArr = new Syllable[this.sylls.length + 1];
+		for (int i = 0; i < this.sylls.length; i++) {
+			newArr[i] = this.sylls[i];
+		}
+		newArr[this.sylls.length] = s;
+		return new SyllableString(newArr);
+	}
+	
+	public boolean endsWith(SyllableString suffix) {
+		int startIndex = this.sylls.length - suffix.sylls.length;
+		if (startIndex < 0) {
+			return false;
+		} else {
+			for (int i = startIndex; i < this.sylls.length; i++) {
+				if (!this.sylls[i].equals(suffix.sylls[i - startIndex])) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+	
+	public void getSounds(int srcBegin, int srcEnd, Syllable[] dst, int dstBegin) {
+		for (int i = srcBegin; i < srcEnd; i++) {
+			dst[i + dstBegin] = this.sylls[i];
+		}
+	}
+	
+	public int indexOf(Syllable s, int start) {
+		for (int i = start; i < this.sylls.length; i++) {
+			if (this.sylls[i].equals(s)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public int indexOf(Syllable s) {
+		return indexOf(s, 0);
+	}
+	
+	public boolean isEmpty() {
+		return sylls.length == 0;
+	}
+	
+	public int lastIndexOf(Syllable s, int offset) {
+		for (int i = this.sylls.length; i >= offset; i--) {
+			if (this.sylls[i].equals(s)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public int lastIndexOf(Syllable s) {
+		return lastIndexOf(s, 0);
+	}
+	
+	public int length() {
+		return this.sylls.length;
+	}
+	
+	public SyllableString replace(Syllable oldS, Syllable newS) {
+		Syllable[] newArr = new Syllable[this.sylls.length];
+		for (int i = 0; i < this.sylls.length; i++) {
+			if (this.sylls[i].equals(oldS)) {
+				newArr[i] = newS;
+			} else {
+				newArr[i] = this.sylls[i];
+			}
+		}
+		
+		return new SyllableString(newArr);
+	}
+	
+	public SyllableString replace(SyllableString oldS, SyllableString newS) {
+		int numFound = 0;
+		for (int i = 0; i < this.sylls.length - oldS.sylls.length; i++) {
+			boolean found = true;
+			for (int j = 0; j < oldS.sylls.length; j++) {
+				if (!this.sylls[i + j].equals(oldS.sylls[j])) {
+					found = false;
+					break;
+				}
+			}
+			if (found) {
+				numFound++;
+			}
+		}
+		
+		Syllable[] newArr = new Syllable[this.sylls.length + (numFound * (newS.sylls.length - oldS.sylls.length))];
+		
+		int currentIndex = 0;
+		for (int i = 0; i < this.sylls.length - oldS.sylls.length; i++) {
+			boolean found = true;
+			for (int j = 0; j < oldS.sylls.length; j++) {
+				if (!this.sylls[i + j].equals(oldS.sylls[j])) {
+					found = false;
+					break;
+				}
+			}
+			if (found) {
+				for (int j = 0; j < newS.sylls.length; j++) {
+					newArr[currentIndex] = newS.sylls[j];
+					currentIndex++;
+				}
+			} else {
+				newArr[currentIndex] = this.sylls[i];
+				currentIndex++;
+			}
+		}
+		
+		return new SyllableString(newArr);
+	}
+	
+	public boolean startsWith(SyllableString s, int offset) {
+		if (s.sylls.length > this.sylls.length - offset) {
+			return false;
+		} else {
+			for (int i = 0; i < s.sylls.length; i++) {
+				if (!s.sylls[i].equals(this.sylls[i + offset])) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+	
+	public boolean startsWith(SyllableString s) {
+		return startsWith(s, 0);
+	}
+	
+	public SyllableString substring(int begin, int end) {
+		Syllable[] newSylls = new Syllable[end - begin];
+		for (int i = begin; i < end; i++) {
+			newSylls[i - begin] = this.sylls[i];
+		}
+		return new SyllableString(newSylls);
+	}
+	
+	public SyllableString substring(int begin) {
+		return substring(begin, this.sylls.length);
+	}
+	
+	public Syllable[] toSyllableArray() {
+		return Arrays.copyOf(sylls, this.sylls.length);
+	}
+	
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		for (int i = 0; i < this.sylls.length; i++) {
+			hash += sylls[i].hashCode() * 31 ^ (this.sylls.length - i - 1);
+		}
+		return hash;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof SyllableString) {
+			SyllableString s = (SyllableString) o;
+			return Arrays.equals(sylls, s.sylls);
+		} else {
+			return false;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < sylls.length; i++) {
+			sb.append(sylls[i].toString());
+			if (i < sylls.length - 1) {
+				sb.append(".");
+			}
+		}
+		return sb.toString();
+	}
+	
 }
